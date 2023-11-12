0e3010ad004da6ffe74d5b222bade5f95aaac327
SiuuIT <emil.burman@gmail.com>
2023-11-12 22:16:30 +0100

    boom

--- src/Volvo.java
+++ src/Volvo.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 class Volvo extends Cars {
     protected Volvo(){
-        Directions direction = Directions.NORTH;
+        direction = Directions.NORTH;
         nrDoors = 4;
         color = Color.black;
         enginePower = 100;
