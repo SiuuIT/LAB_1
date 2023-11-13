5edb6b74efe680bce35581abe39f7b1f23b7d817
Siuu <emilbur@chalmers.se>
2023-11-13 11:39:47 +0100

    börjat på lab2

---
+++ src/Scania.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+public class Scania extends Cars {
+    protected Scania() {
+        direction = Directions.NORTH;
+        nrDoors = 2;
+        color = Color.blue;
+        enginePower = 400;
+        modelName = "temp";
+        stopEngine();
+        angleCheck();
+    }
+
+    private void angleCheck() { // såhär får man nog inte göra
+        while (this.truckBedAngle != 0) {
+            this.currentSpeed = 0;
+        }
+    }
+    private double truckBedAngle = 0;
+
+        @Override
+    public double speedFactor () {
+        return 1;
+        }
+
+    public double getTruckBedAngle () {
+        return this.truckBedAngle;
+    }
+
+    public void raiseAngle ( double amount){
+        boolean b = this.getCurrentSpeed() == 0;
+        if (b) {
+            truckBedAngle += amount;
+            if (truckBedAngle > 70) {
+                truckBedAngle = 70;
+            }
+        }
+    }
+    public void lowerAngle ( double amount){
+        boolean b = this.getCurrentSpeed() == 0;
+        if (b) {
+            truckBedAngle -= amount;
+            if (truckBedAngle < 0) {
+                truckBedAngle = 0;
+            }
+        }
+    }
+}
