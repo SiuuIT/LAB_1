82e07ad52baf186482d7a8d415ae30978a53eefb
Vaenera <leeniley@hotmail.com>
2023-11-16 21:36:33 +0100

    lab2

--- src/Workshop.java
+++ src/Workshop.java
@@ -2,57 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Workshop<T extends Cars> {
-    private List<T> cars;
+    private List<T> carsInWorkshop;
     private final int maxCapacity;
 
     public Workshop(int maxCapacity) {
-        this.cars = new ArrayList<>();
+        this.carsInWorkshop = new ArrayList<>();
         this.maxCapacity = maxCapacity;
     }
 
     private boolean isSpaceAvailable() {
-        return cars.size() < maxCapacity;
+        return carsInWorkshop.size() < maxCapacity;
     }
 
-    public T releaseCar() {
-        if (!cars.isEmpty()) {
-            T releasedCar = cars.remove(cars.size() - 1);
-            System.out.println(releasedCar.getModelName() + " released from the workshop.");
-            return releasedCar;
+    public void receiveCar(T car) {
+        if (isSpaceAvailable()) {
+            carsInWorkshop.add(car);
+            System.out.println(car.getModelName() + " received in the workshop.");
+        } else {
+            System.out.println("Workshop is full.");
+        }
+    }
+
+    public T returnCar() {
+        if (!carsInWorkshop.isEmpty()) {
+            T returnedCar = carsInWorkshop.remove(carsInWorkshop.size() - 1);
+            System.out.println(returnedCar.getModelName() + " returned from the workshop.");
+            return returnedCar;
         } else {
             System.out.println("Workshop is empty.");
             return null;
         }
     }
 
-    public void acceptCarTransport(CarTransport carTransport) {
-        List<Cars> loadedCars = carTransport.getLoadedCars();
-        for (Cars loadedCar : loadedCars) {
-            if (isSpaceAvailable()) {
-                cars.add((T) loadedCar);
-                System.out.println(loadedCar.getModelName() + " accepted in the workshop.");
-            } else {
-                System.out.println("Workshop is full.");
-                break;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        Workshop<Volvo> volvoWorkshop = new Workshop<>(3);
-        Workshop<Saab> saabWorkshop = new Workshop<>(2);
+        Workshop<Volvo> volvoWorkshop = new Workshop<>(5);
+        Workshop<Saab> saabWorkshop = new Workshop<>(5);
 
-        CarTransport carTransport = new CarTransport();
-        carTransport.lowerRamp();
-        carTransport.lassa(new Volvo());
-        carTransport.lassa(new Saab());
+        volvoWorkshop.receiveCar(new Volvo());
+        saabWorkshop.receiveCar(new Saab());
 
-        volvoWorkshop.acceptCarTransport(carTransport);
+        Volvo returnedVolvo = volvoWorkshop.returnCar();
+        Saab returnedSaab = saabWorkshop.returnCar();
 
-        Volvo releasedVolvo = volvoWorkshop.releaseCar();
-        System.out.println("Released Volvo: " + releasedVolvo);
-
-        Saab releasedSaab = saabWorkshop.releaseCar();
-        System.out.println("Released Saab: " + releasedSaab);
+        System.out.println("Returned Volvo: " + returnedVolvo);
+        System.out.println("Returned Saab: " + returnedSaab);
     }
 }
